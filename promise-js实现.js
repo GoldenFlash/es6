182b2c6bc84dbe50d5cff4a20f53c89747294870
wangwei <892458829@qq.com>
2017-12-14 14:06:39 +0800

    add

---
+++ promise-js实现.js
@@ -0,0 +1,35 @@
+ class Promise1 {
+        constructor() {
+            this.callback = [];
+        }
+        resolve(data) {
+            var execute = this.callback.shift();
+            execute.resolve(data)
+        }
+        reject(data) {
+            var execute = this.callback.shift();
+            execute.reject(data)
+        }
+        then(success, fail) {
+            var obj = {
+                resolve: success,
+                reject: fail
+            }
+            this.callback.push(obj)
+            return this;
+        }
+    }
+    var promise = new Promise1();
+
+    function fn() {
+        setTimeout(function() {
+            promise.resolve('data')
+        })
+        return promise;
+    }
+    fn().then(function(data) {
+        console.log(data)
+        promise.resolve()
+    }).then(function() {
+        console.log('fn2')
+    })
